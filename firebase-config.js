783c5652bde2a09be0b156d014bd39d14b5571b2
Jester20-10 <154062869+Jester20-10@users.noreply.github.com>
2026-03-05 19:58:23 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAeZ6DhK-0zECEZlnmwKT6XSFlgHLd7K-w",
+  authDomain: "ural-memory-admin.firebaseapp.com",
+  projectId: "ural-memory-admin",
+  storageBucket: "ural-memory-admin.firebasestorage.app",
+  messagingSenderId: "718188133112",
+  appId: "1:718188133112:web:20994a8d9117ab6ea4590d",
+  measurementId: "G-NR9ZRQ18S6"
+};
