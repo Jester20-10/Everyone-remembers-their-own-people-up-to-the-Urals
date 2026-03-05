43ede881145bd0e1e8291c80e5e9fdf00761d033
Jester20-10 <154062869+Jester20-10@users.noreply.github.com>
2026-03-05 20:25:55 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAeZ6DhK-0zECEZlnmwKT6XSFlgHLd7K-w",
   authDomain: "ural-memory-admin.firebaseapp.com",
@@ -7,3 +11,7 @@ const firebaseConfig = {
   appId: "1:718188133112:web:20994a8d9117ab6ea4590d",
   measurementId: "G-NR9ZRQ18S6"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
